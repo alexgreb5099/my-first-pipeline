a45e68ec079f9b9d2c06b14930493faee3eae0aa
alexgreb5099 <alexgreb5099@gmail.com>
2026-02-28 23:03:48 +0300

    Исправлен тест под новый текст приложения

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 
 def test_homepage():
     response = app.test_client().get('/')
-    assert response.data == b"Hello, DevOps World!"
+    assert response.data == b"Hello, DevOps World! Это мой второй пуск CI"
     assert response.status_code == 200
